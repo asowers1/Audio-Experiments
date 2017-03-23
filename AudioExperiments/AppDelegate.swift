b4b3d3bb5286966ae5d09ea7a15f068d847eb8f1
Andrew Sowers <andrew@skybuds.com>
2017-03-23 17:58:49 -0400

    initial commit

--- AudioExperiments/AppDelegate.swift
+++ AudioExperiments/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        do {
+            try AVAudioSession.sharedInstance().setActive(false, with: .notifyOthersOnDeactivation)
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print(error)
+        }
+        
         return true
     }
 
--- AudioExperiments/ViewController.swift
+++ AudioExperiments/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let audioFiles = ["VO_heather_SU_8", "bluetooth_connect", "bluetooth_search", "connect_fail", "hang_up", "low_battery", "pickup_call01", "play", "power_down"]
+    var player: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        tableView.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func interruped() {
+        print("interrupted")
+        do {
+            try AVAudioSession.sharedInstance().setActive(false, with: .notifyOthersOnDeactivation)
+        } catch {
+            print(error)
+        }
     }
-
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "rate" {
+            if self.player!.rate == 0 {
+                self.interruped()
+            }
+        } else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+        }
+    }
+    
+    //MARK: - TableView -
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print(error)
+        }
+        
+        let sound = URL(fileURLWithPath: Bundle.main.path(forResource: audioFiles[indexPath.row], ofType: "wav")!)
+        player = AVPlayer(url: sound)
+        
+        player?.play()
+        player?.addObserver(self, forKeyPath: "rate", options: NSKeyValueObservingOptions(rawValue: 0), context: nil)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.textLabel?.text = audioFiles[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return audioFiles.count
+    }
 
 }
 
